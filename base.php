af55f3c5600f2f58e24022c37862c4480aa4f15f
Ivy <ivana_lim@dlsu.edu.ph>
2017-11-17 18:08:04 +0800

    ok test

--- base.php
+++ base.php
@@ -6,8 +6,15 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <?php startblock('imports') ?>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
   <link rel="stylesheet" href="style.css" type="text/css"> 
+
+  <link rel="import" href="bower_components/polymer/polymer-element.html">
+	<link rel="import" href="bower_components/iron-ajax/iron-ajax.html">
+	<link rel="import" href="bower_components/paper-button/paper-button.html">
+	<?php endblock() ?>
 </head>
 
 <body>
@@ -75,10 +82,10 @@
 		    </div>
 	  	</div>
 	<?php endblock() ?>
-	
-  
-  
 
+  
+  
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
--- signup.php
+++ signup.php
@@ -2,6 +2,7 @@
 <?php include 'base.php' ?>
 
 <?php startblock('content') ?>
+
   <div class="py-5 text-white opaque-overlay w-100 h-100" style="background-image: url(&quot;homepage bg.jpg&quot;);">
     <div class="container">
       <div class="row h-100">
@@ -9,28 +10,117 @@
         <div class="col-md-6">
           <img class="img-fluid d-block mx-auto" src="T2GO Logo.png" width="350">
           <h1 class="text-gray-dark baloo">Sign Up</h1>
-          <form class="" method="post" action="https://formspree.io/">
+
+          <div class="text-gray-dark baloo" id="feedback"></div>
+<!--
+          <form class="">
             <div class="form-group baloo"> <label class="baloo">Email address</label>
-              <input type="email" name="email" class="form-control" placeholder="Enter email"> </div>
+              <input type="email" id="email" name="email" class="form-control" placeholder="Enter email" required> </div>
+
             <div class="form-group baloo text-white"> <label class="baloo">First Name</label>
-              <input type="text" name="firstname" class="form-control" placeholder="First Name"> </div>
+              <input type="text" id="fname" name="firstname" class="form-control" placeholder="First Name" required> </div>
+
             <div class="form-group baloo"> <label class="baloo">Last Name</label>
-              <input type="text" name="lastname" class="form-control" placeholder="Last Name"> </div>
+              <input type="text" id="lname" name="lastname" class="form-control" placeholder="Last Name" required> </div>
+
             <div class="form-group baloo"> <label class="baloo">Password</label>
-              <input type="password" name="password" class="form-control" placeholder="Create Password"> </div>
+              <input type="password" id="password" name="password" class="form-control" onkeyup='check();' placeholder="Create Password" required> </div>
+
             <div class="form-group baloo"> <label class="baloo">Verify Password</label>
-              <input type="password" name="password" class="form-control" placeholder="Create Password"> </div>
+              <input type="password" id="confirm_password" name="confirm_password" class="form-control" onkeyup='check()' placeholder="Retype Password" required> <span id='message'></span></div>
+              
+
+              <button class="btn btn-primary baloo" onclick="alert();">Sign up</button>
+              <p class="">&nbsp; &nbsp;&nbsp;</p>
+              <p class="">Already have a taft2GO account?&nbsp;</p>
+              <a href="/taft2GO/Login" class="btn btn-outline-primary baloo">Log in</a>
           </form>
-          <a class="btn btn-primary baloo" href="">Sign up</a>
-          <p class="">&nbsp; &nbsp;&nbsp;</p>
-          <p class="">Already have a taft2GO account?&nbsp;</p>
-          <a href="login.html" class="btn btn-outline-primary baloo">Log in</a>
+-->          <button class="btn btn-primary baloo" onclick="createNewAccount();">Sign up</button>
         </div>
       </div>
-      <div class="row">
-        <div class="col-md-12"></div>
-      </div>
+      
     </div>
   </div>
+  <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+  <script src="jquery-3.2.1.min.js"></script>
+<script>
+    //$('#password, #confirm_password').on('keyup', function () { if ($('#password').val() == $('#confirm_password').val()) { $('#message').html('Matching').css('color', 'green'); } else $('#message').html('Not Matching').css('color', 'red'); });
+    
+    function check() {
+      if (document.getElementById('password').value ==
+          document.getElementById('confirm_password').value) {
+          document.getElementById('message').style.color = 'lightgreen';
+          document.getElementById('message').innerHTML = 'matching';
+      } else {
+          document.getElementById('message').style.color = 'red';
+      }
+      
+    }
 
+    function createNewAccount(){
+
+        var email = $('#email').val();
+        var fname = $('#fname').val();
+        var lname = $('#lname').val();
+        var password = $('#password').val();
+        var username = "";
+        var isActive = 1;
+        var isAdmin = 0;
+        var profilePic = "";
+
+        var postData = '{"fname":"'+ fname + ', "lname:"'+ lname + ', "email:"'+ email + ', "password:"'+ password + 
+                        ', "isActive:"'+ isActive + ', "isAdmin:"'+ isAdmin + ', "profilePic:"'+ profilePic + '"}';
+        /*
+        $.ajax({
+          type: "POST",
+          url: "http://127.0.0.1:8080/browser/#/taft2GO/account",
+          processData: false,
+          contentType: "application/json",
+          data: postData,
+          complete: function(jqXHR, exception){
+              console.Log(jqXHR.status);
+              console.Log(jqXHR.responseText);
+
+              if(jqXHR.status == 201){ // created
+                  $('#feedback').html('<h3>Successfully Registered! Please <a href="/taft2GO/Login">Login</a> to continue</h3>');
+                  $('#email').val('');
+                  $('#fname').val('');
+                  $('#lname').val('');
+                  $('#password').val('');
+                  $('#confirm_password').val('');
+              }
+              else if(jqXHR.status == 409){ // conflict
+                $('#feedback').html('<h3>Successfully Registered! Please <a href="/taft2GO/Login">Login</a> to continue</h3>');
+              }
+              else{
+                $('#feedback').html('<h3>Error procesing request.</h3>');
+              }
+          }
+
+        });
+        */
+
+        $.ajax({
+          type: "POST",
+          url: "#",
+          data: "id=",
+          dataType: "html",
+          success: function(result){
+          //$('#change').html(result);
+          alert();
+         // $('#ajaxPostal').html(result);
+          }
+        });
+    }
+
+    /*
+    $(document).ready(function() {
+
+      $('#').click(function () { // Stop movement
+            
+        });
+
+    });
+*/
+</script>
 <?php endblock() ?>
