880ef6310d7b09750b1703a0710a07d4bbbe1627
Ivy <ivana_lim@dlsu.edu.ph>
2017-11-17 22:14:11 +0800

    ok test

--- signup.php
+++ signup.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <?php include 'base.php' ?>
 
+
 <?php startblock('content') ?>
 
   <div class="py-5 text-white opaque-overlay w-100 h-100" style="background-image: url(&quot;homepage bg.jpg&quot;);">
@@ -12,8 +13,8 @@
           <h1 class="text-gray-dark baloo">Sign Up</h1>
 
           <div class="text-gray-dark baloo" id="feedback"></div>
-<!--
-          <form class="">
+
+          <!--<form class="">--><!---->
             <div class="form-group baloo"> <label class="baloo">Email address</label>
               <input type="email" id="email" name="email" class="form-control" placeholder="Enter email" required> </div>
 
@@ -30,21 +31,20 @@
               <input type="password" id="confirm_password" name="confirm_password" class="form-control" onkeyup='check()' placeholder="Retype Password" required> <span id='message'></span></div>
               
 
-              <button class="btn btn-primary baloo" onclick="alert();">Sign up</button>
+              <!--<button class="btn btn-primary baloo" id="test">Sign up</button>-->
+              <button class="btn btn-primary baloo" type="submit" onclick="createNewAccount();">Sign up</button>
               <p class="">&nbsp; &nbsp;&nbsp;</p>
               <p class="">Already have a taft2GO account?&nbsp;</p>
               <a href="/taft2GO/Login" class="btn btn-outline-primary baloo">Log in</a>
           </form>
--->          <button class="btn btn-primary baloo" onclick="createNewAccount();">Sign up</button>
+          
         </div>
       </div>
       
     </div>
   </div>
-  <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
-  <script src="jquery-3.2.1.min.js"></script>
+
 <script>
-    //$('#password, #confirm_password').on('keyup', function () { if ($('#password').val() == $('#confirm_password').val()) { $('#message').html('Matching').css('color', 'green'); } else $('#message').html('Not Matching').css('color', 'red'); });
     
     function check() {
       if (document.getElementById('password').value ==
@@ -70,16 +70,16 @@
 
         var postData = '{"fname":"'+ fname + ', "lname:"'+ lname + ', "email:"'+ email + ', "password:"'+ password + 
                         ', "isActive:"'+ isActive + ', "isAdmin:"'+ isAdmin + ', "profilePic:"'+ profilePic + '"}';
-        /*
+        
         $.ajax({
           type: "POST",
-          url: "http://127.0.0.1:8080/browser/#/taft2GO/account",
+          url: "http://127.0.0.1:8080/browser/taft2GO/account",
           processData: false,
           contentType: "application/json",
           data: postData,
           complete: function(jqXHR, exception){
-              console.Log(jqXHR.status);
-              console.Log(jqXHR.responseText);
+              console.log(jqXHR.status);
+              console.log(jqXHR.responseText);
 
               if(jqXHR.status == 201){ // created
                   $('#feedback').html('<h3>Successfully Registered! Please <a href="/taft2GO/Login">Login</a> to continue</h3>');
@@ -98,19 +98,7 @@
           }
 
         });
-        */
-
-        $.ajax({
-          type: "POST",
-          url: "#",
-          data: "id=",
-          dataType: "html",
-          success: function(result){
-          //$('#change').html(result);
-          alert();
-         // $('#ajaxPostal').html(result);
-          }
-        });
+        
     }
 
     /*
